9f3cc8c17cad867afc32d3ec1f949097121e69e3
PrinssiFiestas <hik.lorenzo@hotmail.com>
2022-12-06 18:34:33 +0200

    first commit

---
+++ file2string.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "file2string.h"
+
+static FILE* fopenWithErrorHandling(const char* filePath, const char* sourceFile, int line)
+{
+	// Suppres unused parameter warnings
+	(void)(sourceFile); (void)(line);
+
+	FILE* file;
+
+	// At the time of writing MSVC requires fopen_s for whatever reason
+#if defined(_MSC_VER) && (_MSC_VER >= 1400 ) && (!defined WINCE)
+	int err = fopen_s(&file, filePath, "r");
+	if(err == 0)
+#else
+	file = fopen(filePath, "r");
+	if(file == NULL)
+#endif
+	{
+#ifdef PRINT_FILE_OPENING_FAILURE_MESSAGE_TO_STDOUT
+		printf("Error opening file \"%s\" on \"%s\" line %i!\n", filePath, sourceFile, line);
+#endif
+#ifdef EXIT_ON_FILE_OPENING_FAILURE
+		exit(EXIT_FAILURE);
+#endif
+	}
+	return file;
+}
+
+long _sizeOfFile(const char* filePath, const char* sourceFile, int line)
+{
+	FILE* file = fopenWithErrorHandling(filePath, sourceFile, line);
+	if(file == NULL)
+		return -1;
+
+	fseek(file, 0, SEEK_END);
+	long fileSize = ftell(file);
+	fclose(file);
+	
+	return fileSize;
+}
+
+char* _readFileToStr(const char* filePath, char* outputStr, const char* sourceFile, int line)
+{
+	FILE* file = fopenWithErrorHandling(filePath, sourceFile, line);
+	if(file == NULL)
+		return NULL;
+
+	fseek(file, 0, SEEK_END);
+	long fileSize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	fread(outputStr, fileSize, 1, file);
+	outputStr[fileSize] = '\0';
+	fclose(file);
+
+	return outputStr;
+}
+
+char* _mallocAndReadFile(const char* filePath, const char* sourceFile, int line)
+{
+	FILE* file = fopenWithErrorHandling(filePath, sourceFile, line);
+	if(file == NULL)
+		return NULL;
+
+	fseek(file, 0, SEEK_END);
+	long fileSize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	char* fileContents = (char*)malloc(fileSize * sizeof(char));
+	fread(fileContents, fileSize, 1, file);
+	fileContents[fileSize] = '\0';
+	fclose(file);
+	
+	return fileContents;
+}
+
---
+++ file2string.h
@@ -0,0 +1,44 @@
+#ifndef FILE2STRING_H
+#define FILE2STRING_H
+
+// ------------------------------------
+// Settings
+
+//#define PRINT_FILE_OPENING_FAILURE_MESSAGE_TO_STDOUT
+//#define EXIT_ON_FILE_OPENING_FAILURE
+
+// ------------------------------------
+
+#define readFile(FILE_PATH)	\
+	readFileToStr(FILE_PATH, (char*)alloca(sizeOfFile(FILE_PATH) + 1/*null termination*/))
+
+#define sizeOfFile(FILE_PATH)	\
+	_sizeOfFile(FILE_PATH, __FILE__, __LINE__)
+
+#define readFileToStr(FILE_PATH, OUTPUT_STR)	\
+	_readFileToStr(FILE_PATH, OUTPUT_STR, __FILE__, __LINE__)
+
+#define mallocAndReadFile(FILE_PATH)	\
+	_mallocAndReadFile(FILE_PATH, __FILE__, __LINE__)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Returns -1 on failure
+long _sizeOfFile(const char* filePath, const char* sourceFile, int line);
+
+// Returns NULL on failure
+char* _readFileToStr(const char* filePath, char* outputString, const char* sourceFile, int line);
+
+// Returns NULL on failure
+char* _mallocAndReadFile(const char* filePath, const char* sourceFile, int line);
+
+// ------------------------------------
+
+#ifdef __cplusplus
+}
+#endif // extern "C"
+
+#endif // FILE2STRING_H
+
